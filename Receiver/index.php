2c5eb485945043bc1fde4982eafda41abebed99a
JaredJimenez <atomh@live.com>
2022-07-18 14:46:02 -0500

    Upda

--- Receiver/index.php
+++ Receiver/index.php
@@ -4,6 +4,7 @@ $host = "localhost";
 $usuario = "root";
 $password = "";
 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=prueba", $usuario, $password);      
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
